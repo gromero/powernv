6470889b699ead293a745e666f09b38852ac5592
Gustavo Romero <gusbromero@gmail.com>
2019-10-09 12:44:40 -0300

    Initial commit

---
+++ Makefile
@@ -0,0 +1,44 @@
+CC=gcc
+LD=ld
+OBJCOPY=objcopy
+QEMU=~/qemu/bin/qemu-system-ppc64
+QEMU_CMD=$(QEMU) -s -M powernv -cpu POWER9 -nographic -bios ./image.bin
+
+LDFLAGS = -N -T powernv.lds
+
+SRC_C := uart.c
+SRC_S := h.S
+
+OBJ_C := $(SRC_C:.c=.o)
+OBJ_S := $(SRC_S:.S=.o)
+
+all: image
+	@size image.elf
+
+	@echo "image.elf mapping:"
+	@cat image.map
+
+image.elf: $(OBJ_C) $(OBJ_S) powernv.lds
+	$(LD) $(OBJ_C) $(OBJ_S) $(LDFLAGS) -o $@
+
+image.map: image.elf
+	nm image.elf | sort > image.map
+
+image.bin: image.elf
+	$(OBJCOPY) -O binary $^ $@
+
+image:	image.bin image.map
+
+$(OBJ_C):	$(SRC_C)
+	$(CC) $^ -c -o $@
+
+$(OBJ_S):	$(SRC_S)
+	$(CC) $^ -c -o $@
+
+clean:
+	rm -fr *.o
+	rm -fr image.*
+
+run: all
+	which $(QEMU)
+	$(QEMU_CMD)
---
+++ uart.c
@@ -0,0 +1,107 @@
+#include <unistd.h>
+#include <stdbool.h>
+#include <inttypes.h>
+
+#define QEMU_UART_BASE 0x60300d00103f8
+
+/* Taken from skiboot. TODO: where is the documentation?! */
+#define REG_RBR		0
+#define REG_THR		0
+#define REG_DLL		0
+#define REG_IER		1
+#define REG_DLM		1
+#define REG_FCR		2
+#define REG_IIR		2
+#define REG_LCR		3
+#define REG_MCR		4
+#define REG_LSR		5
+#define REG_MSR		6
+#define REG_SCR		7
+
+#define LSR_DR		0x01  /* Data ready */
+#define LSR_OE		0x02  /* Overrun */
+#define LSR_PE		0x04  /* Parity error */
+#define LSR_FE		0x08  /* Framing error */
+#define LSR_BI		0x10  /* Break */
+#define LSR_THRE	0x20  /* Xmit holding register empty */
+#define LSR_TEMT	0x40  /* Xmitter empty */
+#define LSR_ERR		0x80  /* Error */
+
+#define LCR_DLAB 	0x80  /* DLL access */
+
+#define IER_RX		0x01
+#define IER_THRE	0x02
+#define IER_ALL		0x0f
+
+static uint64_t qemu_uart_base = QEMU_UART_BASE;
+
+static void qemu_uart_reg_write(uint64_t offset, uint8_t val)
+{
+	uint64_t addr;
+
+	addr = qemu_uart_base + offset;
+	*(volatile uint8_t *)addr = val;
+}
+
+static uint8_t qemu_uart_reg_read(uint64_t offset)
+{
+	uint64_t addr;
+	uint8_t val;
+
+	addr = qemu_uart_base + offset;
+	val = *(volatile uint8_t *)addr;
+
+	return val;
+}
+
+static int qemu_uart_tx_full(void)
+{
+	return !(qemu_uart_reg_read(REG_LSR) & LSR_THRE);
+}
+
+static int qemu_uart_rx_empty(void)
+{
+	return !(qemu_uart_reg_read(REG_LSR) & LSR_DR);
+}
+
+static char qemu_uart_read(void)
+{
+	return qemu_uart_reg_read(REG_THR);
+}
+
+static void qemu_uart_write(char c)
+{
+	qemu_uart_reg_write(REG_RBR, c);
+}
+
+// Receive a single character
+int uart_receive_char(void) {
+    unsigned char c = 0;
+    while (qemu_uart_rx_empty()) ;
+    c = qemu_uart_read();
+    return c;
+}
+
+// Send string of a given length
+void uart_send_string(const char *str, int len) {
+	int i;
+	for (i = 0; i < len; i++) {
+		while(qemu_uart_tx_full());
+		qemu_uart_write(str[i]);
+	}
+}
+
+int main() {
+  // FTD (r3) is saved to r14 in h.S
+  register char *fdtp __asm__ ("r14");
+  char msg[]="\n----\n";
+
+  // Dump 4k from device tree
+  uart_send_string(fdtp, 1024*4);
+
+  // Print some mark in the end
+  uart_send_string(&msg[0], 6);
+
+  // hal
+  while (1) ;
+}
